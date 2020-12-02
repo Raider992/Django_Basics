771173eea56668fe7c525f189113e9037195e2ca
Raider992 <19stargazer92@gmail.com>
2020-12-02 22:04:55 +0300

    Докинул что написал на 2 уроке, вторую ветку удалил

--- mainapp/views.py
+++ mainapp/views.py
@@ -8,3 +8,18 @@ def index(request):
 def products(request):
     return render(request, 'mainapp/products.html')
 
+def test_context(request):
+    context = {
+        'title': 'добро пожаловать',
+        'username': 'This User',
+        'products':[
+            {'name':'111', 'price':'222'},
+            {'name': '121', 'price': '221'},
+            {'name': '112', 'price': '223'}
+        ],
+        'promotion': True,
+        'promotion_products':[
+            {'name': '121', 'price': '221'}
+        ]
+    }
+    return render(request, 'mainapp/context.html', context)
